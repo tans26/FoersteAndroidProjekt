923a32d2c0f4c3e365c6ca822e6663132cc51982
Tanja Sølvsten <tanjasolvsten@live.dk>
2019-09-24 17:02:09 +0200

    onClick Metode

--- app/src/main/java/com/example/foersteprojekt/MainActivity.java
+++ app/src/main/java/com/example/foersteprojekt/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.foersteprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView tiden = findViewById(R.id.aktuelTid);
+
+        String tidTekst = "" + new Date();
+
+        tiden.setText(tidTekst);
+
+
+        Button b = findViewById(R.id.knappen);
+
+        b.setText("Tryk på mig");
+
+        b.setOnClickListener(this);
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+
     }
 }
