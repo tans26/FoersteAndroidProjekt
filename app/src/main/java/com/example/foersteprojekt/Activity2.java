a033bbff0d541a7912c6fec4a00068480a8f5bcd
Tanja Sølvsten <tanjasolvsten@live.dk>
2019-09-25 19:38:55 +0200

    Tilføjet knapper på Activity2

--- app/src/main/java/com/example/foersteprojekt/Activity2.java
+++ app/src/main/java/com/example/foersteprojekt/Activity2.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity implements View.OnClickListener {
+
+    Button button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +19,31 @@ public class Activity2 extends AppCompatActivity {
 
 
         Intent intent = getIntent();
-        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+        String text = intent.getStringExtra(MainActivity.NEXT_TEXT);
 
         TextView textView1 = findViewById(R.id.textView1);
 
-        textView1.setText(text);
+        textView1.setText("Velkommen " + text + "!");
+
+
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        System.out.println("Der blev trykket på en knap");
+
+        if (v == button2) {
+
+            button2.setText("Du trykkede på mig. Tak! \n");
+
+        } else if (v == button3) {
+
+            button3.setText("Du trykkede på mig. Tak!\n");
+
+        }
     }
 }
--- app/src/main/java/com/example/foersteprojekt/MainActivity.java
+++ app/src/main/java/com/example/foersteprojekt/MainActivity.java
@@ -7,55 +7,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_TEXT = "com.example.foersteprojekt.EXTRA_TEXT";
-
-    //String name;
-
-    //EditText nameInput;
-
+    public static final String NEXT_TEXT = "com.example.foersteprojekt.NEXT_TEXT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //nameInput = (EditText) findViewById(R.id.nameInput);
-
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //name = nameInput.getText().toString();
-                Insend();
-                //showToast(name);
-
+                Indsend();
             }
         });
 
     }
 
-    public void Insend(){
+    public void Indsend(){
         EditText editText1 = findViewById(R.id.nameInput);
         String text = editText1.getText().toString();
 
 
         Intent intent = new Intent (this, Activity2.class);
-        intent.putExtra(EXTRA_TEXT, text);
+        intent.putExtra(NEXT_TEXT, text);
 
         startActivity(intent);
+        System.out.println("Der blev trykket");
 
 
 
     }
-
-
-    //private void showToast(String text){
-        //Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
-
-
-    }
-//}
+}
